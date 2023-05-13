f98c1170deb1f39d3e6b88771ab1f60734a7cd1b
DavyHaYehoudi <davyhayehoudi@gmail.com>
2023-05-13 15:02:20 +0300

    tentative modale mais apparait en bas du body

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,29 +1,66 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export function Modals(props) {
+  const { item } = props;
   return (
     <Modal
       {...props}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      className="showInfos"
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+      <Modal.Header closeButton className="infosContent">
+        <Modal.Title id="contained-modal-title-vcenter" className="head">
+            <h2>{item.name}</h2>
+            {item.demo && (
+              <div className="sourceCode">
+                <a
+                  href={item.demo}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-video"></i>
+                </a>
+              </div>
+            )}
+            {item.source && (
+              <div className="sourceCode">
+                <a
+                  href={item.source}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  Source
+                </a>
+              </div>
+            )}
+            {item.link && (
+              <div className="sourceCode">
+                <a
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-link"></i>
+                </a>
+              </div>
+            )}
+          
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-         coucou les amis, je suis ici !
-        </p>
+        <p className="text">{item.info}</p>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={props.onHide} className="button return">
+          Fermer
+        </Button>
       </Modal.Footer>
     </Modal>
   );
 }
-
--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Modals } from "../Modals";
+import Modal from "react-bootstrap/Modal";
+import Button from 'react-bootstrap/Button';
+
 
 const Project = ({ item }) => {
   // const [showInfo, setShowInfo] = useState(false);
@@ -9,81 +10,148 @@ const Project = ({ item }) => {
   //   setShowInfo(!showInfo);
   // };
   const [modalShow, setModalShow] = useState(false);
+  console.log("show modal :",modalShow);
 
   return (
     <>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        Launch vertically centered modal
-      </Button>
+    <div className="project" onClick={() => setModalShow(true)}>
+    
+        <div className="icons">
+          {item.languagesIcons.map((icon) => (
+            <i className={icon} key={icon}></i>
+          ))}
+        </div>
+        <h3>{item.name}</h3>
+        <img src={item.picture} alt={item.picture} />
+        <span className="infos">
+          <i class="fa-sharp fa-solid fa-circle-info"></i>
+        </span>
 
-      <Modals
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+
+    </div>
+    <Modal
+      // size="lg"
+      // aria-labelledby="contained-modal-title-vcenter"
+      // centered
+      // className="showInfos"
+      onHide={() => setModalShow(false)}
+      show={modalShow} 
+    >
+      <Modal.Header closeButton className="infosContent">
+        <Modal.Title id="contained-modal-title-vcenter" className="head">
+            <h2>{item.name}</h2>
+            {item.demo && (
+              <div className="sourceCode">
+                <a
+                  href={item.demo}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-video"></i>
+                </a>
+              </div>
+            )}
+            {item.source && (
+              <div className="sourceCode">
+                <a
+                  href={item.source}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  Source
+                </a>
+              </div>
+            )}
+            {item.link && (
+              <div className="sourceCode">
+                <a
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  className="button"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-link"></i>
+                </a>
+              </div>
+            )}
+          
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p className="text">{item.info}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => setModalShow(false)} className="button return">
+          Fermer
+        </Button>
+      </Modal.Footer>
+    </Modal>
+ 
     </>
   );
-  return (
-    // <div className="project">
-    //   <div className="icons">
-    //     {item.languagesIcons.map((icon) => (
-    //       <i className={icon} key={icon}></i>
-    //     ))}
-    //   </div>
-    //   <h3>{item.name}</h3>
-    //   <img src={item.picture} alt={item.picture} onClick={handleInfo} />
-    //   <span className="infos" onClick={handleInfo}>
-    //   <i class="fa-sharp fa-solid fa-circle-info"></i>     
-    //    </span>
+  // return (
+  //   // <div className="project">
+  //   //   <div className="icons">
+  //   //     {item.languagesIcons.map((icon) => (
+  //   //       <i className={icon} key={icon}></i>
+  //   //     ))}
+  //   //   </div>
+  //   //   <h3>{item.name}</h3>
+  //   //   <img src={item.picture} alt={item.picture} onClick={handleInfo} />
+  //   //   <span className="infos" onClick={handleInfo}>
+  //   //   <i class="fa-sharp fa-solid fa-circle-info"></i>
+  //   //    </span>
 
-    //   {showInfo && (
-    //     <div className="showInfos">
-    //       <div className="infosContent">
-    //         <div className="head">
-    //           <h2>{item.name}</h2>
-    //           {item.demo && (
-    //             <div className="sourceCode">
-    //               <a
-    //                 href={item.demo}
-    //                 rel="noopener noreferrer"
-    //                 className="button"
-    //                 target="_blank"
-    //               >
-    //                 <i className="fa-solid fa-video" ></i>
-    //               </a>
-    //             </div>
-    //           )}
-    //           {item.source&&<div className="sourceCode">
-    //             <a
-    //               href={item.source}
-    //               rel="noopener noreferrer"
-    //               className="button"
-    //               target="_blank"
-    //             >
-    //               Source
-    //             </a>
-    //           </div>}
-    //           {item.link&&<div className="sourceCode">
-    //             <a
-    //               href={item.link}
-    //               rel="noopener noreferrer"
-    //               className="button"
-    //               target="_blank"
-    //             >
-    //               <i className="fa-solid fa-link" ></i>
-    //             </a>
-    //           </div>}
-    //         </div>
-    //         <p className="text">{item.info}</p>
+  //   //   {showInfo && (
+  //   //     <div className="showInfos">
+  //   //       <div className="infosContent">
+  //   //         <div className="head">
+  //   //           <h2>{item.name}</h2>
+  //   //           {item.demo && (
+  //   //             <div className="sourceCode">
+  //   //               <a
+  //   //                 href={item.demo}
+  //   //                 rel="noopener noreferrer"
+  //   //                 className="button"
+  //   //                 target="_blank"
+  //   //               >
+  //   //                 <i className="fa-solid fa-video" ></i>
+  //   //               </a>
+  //   //             </div>
+  //   //           )}
+  //   //           {item.source&&<div className="sourceCode">
+  //   //             <a
+  //   //               href={item.source}
+  //   //               rel="noopener noreferrer"
+  //   //               className="button"
+  //   //               target="_blank"
+  //   //             >
+  //   //               Source
+  //   //             </a>
+  //   //           </div>}
+  //   //           {item.link&&<div className="sourceCode">
+  //   //             <a
+  //   //               href={item.link}
+  //   //               rel="noopener noreferrer"
+  //   //               className="button"
+  //   //               target="_blank"
+  //   //             >
+  //   //               <i className="fa-solid fa-link" ></i>
+  //   //             </a>
+  //   //           </div>}
+  //   //         </div>
+  //   //         <p className="text">{item.info}</p>
 
-    //         <div className="button return" onClick={handleInfo}>
-    //           Back
-    //         </div>
-    //       </div>
-    //     </div>
-    //   )}
-    // </div>
-    <Modals />
-  );
+  //   //         <div className="button return" onClick={handleInfo}>
+  //   //           Back
+  //   //         </div>
+  //   //       </div>
+  //   //     </div>
+  //   //   )}
+  //   // </div>
+  // );
 };
 
 export default Project;
